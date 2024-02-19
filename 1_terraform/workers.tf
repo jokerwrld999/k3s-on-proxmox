e6b27420cf941eed0e7f8c37fe1ccb6cddcad8e4
jokerwrld999 <ifalaleev49@gmail.com>
2024-02-19 10:28:44 +0200

    [terraform] Ansible Inventory Generation added

--- 1_terraform/workers.tf
+++ 1_terraform/workers.tf
@@ -51,13 +51,23 @@ resource "proxmox_vm_qemu" "k3s_worker" {
 
   # (Optional) IP Address and Gateway
   ipconfig0  = "ip=10.10.10.6${count.index + 1}/24,gw=10.10.10.254"
-  nameserver = "10.10.10.254"
+  nameserver = "1.1.1.1"
 
   # (Optional) Default User
   ciuser = "jokerwrld"
 
   # (Optional) Add your SSH KEY
-  sshkeys = <<EOF
-  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCz4DdmN/mKihH/76hXc6RAzb5Du0dE77PfHSOUbAcEqtstkZIhZpwJAOKo0H4yfVIRHZh8Tu83lCcNZ0ewQ+3aDgbpISSGqD0be5jazLMQCP304/iJr5Kz79scZSZc4h8vQg1gZrKyIUvbg84usYn3oai4U0eDQxgkDaRO/TS/u+wNjKSEmUkc+iytYF53jqhJApIJfDeATuO+o5AKa7ccO5LPp4RPa6XeKOX0BORxhuKS4y0i0jz2SgeXtCcQ1iLSFyaQeOL5dzxUDU5sRZf7BcQ/gVscPNCMTsT+e4in0ubdCvG9W4oeUK3RzypoFNzmby8jjbezci5XBsJm/SOg/ELjN9V4p4lxfVFQlhNkIgO0dDIvXD5CSsxE8Nhn9toc2YC6/7sPOKvQCTQOcNOKgBJR6jARQT5Ktfe3HA2yWHI4MOUOzr8d2tgKj14aPrj5kB7D6Jl7RG2kvCfmQrF82oAemguCI2qskxlX2nUDFuIjQOSs8pO/5llWQ9YJQA4M7Q2ypaPFg0DdMmwlShvjfIqAe58TRsoY/aHQ0/2NwbDiPMWl38v0lCKvk3kWNxca5/Nz9hfAEnQvQLSDqjgsXdbvaMtZWdnQf8DYmKVrXj8v/7k3B7tocEOCMA+xwH/gB8cSRHH7dAtB2CJIdIuxIj0PcBTNzJih+S5a2iH2+w==
+  sshkeys = <<-EOF
+    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDytT6H5MCg3PZ55d+SmPJ5xUUKYyINFyeCst56c8wXInX895eNpNdmcaupDZRCacRQHIDRkOPj7rstolOXg8KgDc5tJ8JaGVEKGsnkY11P9JeTR7fWia7S7gwKSDQWPeTh/x5vxVFeuInErVBwPJFaebiV02MkA7OalFQ1DiHQvne4EtLYEp9/WTZdrDQw2sZfiUuh9KS9YSeWkB+TgK3mk8tmLGR7FO5WMLQbnZZu0nD7Q46i5xNQy3N5DOR1cVispfqfKZSAw1WubjqeZodknJkxWUZKjI3ebOKbYTT5fwHkOJu3jgN7kZXx6pq2cujKnseFEm0yPbSbXZVCl2I8RuvWo7hA/PdkPYzwTSbWEEeZaEfajUc1LAV4xMTYHdvsxCcMEuiZL+YxbfCWSlcg0xJpFf2hj+XiZTaRdpW9p1BYiYqcLQJYFNXV8s0YgpXDXz4oQPcXbzoJc5HZU3A3PnF1u8ElZmCyxRKxuoueb0fvFRD/mZ0cjJkJmA9e5Js=
+  EOF
+}
+
+resource "local_file" "k3s_worker_nodes" {
+  filename = "../2_ansible/inventory/k3s_worker_nodes"
+  content = <<-EOF
+    [k3s_worker_nodes]
+    %{ for ip in proxmox_vm_qemu.k3s_worker ~}
+    ${regex("[0-9.]+", ip.ipconfig0)}
+    %{ endfor ~}
   EOF
 }
